c1f31da57ce168307bc394dcb40af734b2e29fb0
kzri4 <kazu4126ab@gmail.com>
2023-04-22 19:26:22 +0900

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\PlanetControlle::class, 'index']);
+Route::get('/', [App\Http\Controllers\PlanetController::class, 'index']);
 
 Route::resource('planets', App\Http\Controllers\PlanetController::class);
